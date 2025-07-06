114268f24b4df7a3c63b6b4c8cd18ca7ddb93d9f
Andrew Patynko <andriy.patinko@gmail.com>
2025-07-06 10:19:29 +0300

    Create hello.py

---
+++ python/hello.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompt the user for their name
+    name = input("What is your name?\n")
+
+    # Greet the user
+    print(f"hello, {name}")
+
+if __name__ == "__main__":
+    main()
